71cdc206cbeae68fb083c85079846efff71141bd
John Gilbert <johngilb@gmail.com>
2015-12-05 14:34:13 -0800

    Working with some tests. Not fully tested yet, but getting there.

--- src/xbot/common/subsystems/PoseSubsystem.java
+++ src/xbot/common/subsystems/PoseSubsystem.java
@@ -19,23 +19,15 @@ public class PoseSubsystem {
     public XGyro gyro;
     public XEncoder leftLongitudinalEncoder;
     public XEncoder rightLongitudinalEncoder;
-    public XEncoder middleTransverseEncoder;
+    public XEncoder middleTransverseEncoder;    
     
+    protected XYPair fieldPosition;
+    protected XYPair fieldVelocity;
+    protected XYPair robotTilt;
     
-    protected double robotXPosition;
-    protected double robotYPosition;
-    
-    protected double robotXVelocity;
-    protected double robotYVelocity;
-    
-    protected double robotTransverseTilt;
-    protected double robotLongitundinalTilt;
-    
-    
-    private double previousTotalLongitudinalDistanceX_Left;
-    private double previousTotalLongitudinalDistanceX_Right;
-    private double previousTotalLongitudinalDistanceY;
-    
+    private double previousTotalLongitudinalDistance_Left;
+    private double previousTotalLongitudinalDistance_Right;
+    private double previousTotalTransverseDistance;
     
     protected double gyroYawOffset = 0;
     protected double FORWARD_ANGLE = 90;
@@ -46,28 +38,72 @@ public class PoseSubsystem {
         this.deviceFactory = deviceFactory;
         setupSensors();
         calibrate(FORWARD_ANGLE);
+        
+        fieldPosition = new XYPair();
+        fieldVelocity = new XYPair();
+        robotTilt = new XYPair();
     }
     
+    /**
+     * Allocates the sensors that this subsystem needs to discover position, rotation, velocity, and tilt.
+     * If you use different sensors, override this method.
+     */
     public void setupSensors()
     {
         gyro = deviceFactory.getGyro();
+
+        leftLongitudinalEncoder = deviceFactory.getEncoder(0, 1);
+        rightLongitudinalEncoder = deviceFactory.getEncoder(2, 3);
+        middleTransverseEncoder = deviceFactory.getEncoder(4, 5);
     }
     
     /**
      * Updates all base sensor data related to robot pose. This method needs to be called frequently 
      * and ideally periodically.
      * 
-     * It needs to set the following protected variables:
-     *     robotXPosition
-     *     robotYPosition
-     *     robotXVelocity
-     *     robotYVelocity
-     *     robotTransverseTilt
-     *     robotLongitundinalTilt
+     * It needs to set the following protected variables (all field-relative):
+     *     robotPosition
+     *     robotVelocity
+     *     robotTilt
      */
     public void updateSensorValues()
     {
+        double vDist1 = leftLongitudinalEncoder.getDistance() - previousTotalLongitudinalDistance_Left;
+        double vDist2 = rightLongitudinalEncoder.getDistance() - previousTotalLongitudinalDistance_Right;
+        double hDist = middleTransverseEncoder.getDistance() - previousTotalTransverseDistance;
         
+        XYPair distSinceLastTick = new XYPair(hDist, (vDist1 + vDist2) / 2);
+        
+        //Rotate vector from robot- to field-relative
+        distSinceLastTick.rotate(getCalibratedGyroYaw().getValue() - FORWARD_ANGLE);
+        
+        fieldPosition.add(distSinceLastTick);
+        
+        updatePrevTotals();
+        
+        double vVel1 = leftLongitudinalEncoder.getRate();
+        double vVel2 = rightLongitudinalEncoder.getRate();
+        double hVel = middleTransverseEncoder.getRate();
+        
+        fieldVelocity.y = (vVel1 + vVel2) / 2;        
+        fieldVelocity.x = hVel;
+        
+        fieldVelocity = fieldVelocity.rotate(getCalibratedGyroYaw().getValue() - FORWARD_ANGLE);
+        
+        // tilt detection
+        double pitch = gyro.getPitch();
+        double roll = gyro.getRoll();
+        
+        robotTilt = new XYPair(roll, pitch);
+        
+        // now that we have all these values, update those key variables
+    }
+    
+    private void updatePrevTotals()
+    {
+        previousTotalLongitudinalDistance_Left = leftLongitudinalEncoder.getDistance();
+        previousTotalLongitudinalDistance_Right = rightLongitudinalEncoder.getDistance();
+        previousTotalTransverseDistance = middleTransverseEncoder.getDistance();
     }
     
     public ContiguousDouble getUncalibratedGyroYaw()
@@ -101,33 +137,57 @@ public class PoseSubsystem {
         log.info("Calibrating yaw offset to: " + gyroYawOffset);
     }
 
+    /**
+     * Returns robot tilt, with roll as X and pitch as Y.
+     * @return
+     */
     public XYPair getTilt() {
-        // TODO Auto-generated method stub
-        return new XYPair(0,0);
+        updateSensorValues();
+        return robotTilt;
     }
 
+    /**
+     * Returns the maximum safe tilt value - experimentally derived. The idea being that if tilt >
+     * max tilt, you may want to take immediate corrective action.
+     * @return
+     */
     public XYPair getMaxTilt() {
-        // TODO Auto-generated method stub
         return new XYPair(1,1);
     }
 
+    /**
+     * Returns the field-oriented position of the robot.
+     * @return
+     */
     public XYPair getPosition() {
-        // TODO Auto-generated method stub
-        return new XYPair(0,0);
+        updateSensorValues();
+        return fieldPosition;
     }
 
     public XYPair getVelocityFieldRelative() {
-        // TODO Auto-generated method stub
-        return new XYPair(0,0);
+        updateSensorValues();
+        return fieldVelocity;
     }
 
     public XYPair getVelocityRobotRelative() {
-        // TODO Auto-generated method stub
-        return new XYPair(0,0);
+        updateSensorValues();
+        // need to rotate velocity back towards robot
+        XYPair robotVelocity = fieldVelocity.rotate(FORWARD_ANGLE - getCalibratedGyroYaw().getValue());
+        return robotVelocity;
     }
 
+    /**
+     * Compares the robot's current velocity to the given tolerance - if it is below, we consider the robot
+     * to be "stopped."
+     * @param velTolerance
+     * @return
+     */
     public boolean isStopped(double velTolerance) {
-        // TODO Auto-generated method stub
+        updateSensorValues();
+        if (fieldVelocity.getMagnitude() < velTolerance)
+        {
+            return true;
+        }
         return false;
     }
     
--- tests/xbot/common/subsystems/PoseTests.java
+++ tests/xbot/common/subsystems/PoseTests.java
@@ -2,9 +2,12 @@ package xbot.common.subsystems;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
+import edu.wpi.first.wpilibj.MockEncoder;
+import static org.junit.Assert.assertEquals;
+import xbot.common.controls.sensors.MockGyro;
 import xbot.common.injection.BaseWPITest;
+import xbot.common.math.XYPair;
 
 public class PoseTests extends BaseWPITest {
     
@@ -55,5 +58,101 @@ public class PoseTests extends BaseWPITest {
         mockRobotIO.setGyroHeading(180);
         assertEquals(pose.FORWARD_ANGLE+180-90, pose.getCalibratedGyroYaw().getValue(), 0.001);
     }
+    
+    @Test
+    public void checkGoForward() {
+        setDistances(10, 10, 0);
+        checkDistances(0, 10);
+        
+        setDistances(-10, -10, 0);
+        checkDistances(0, -10);
+    }
+    
+    @Test
+    public void checkStrafe() {
+        setDistances(0, 0, 10);
+        checkDistances(10, 0);
+        
+        setDistances(0, 0, -10);
+        checkDistances(-10, 0);
+    }
+    
+    @Test
+    public void checkRotateThenGoForward() {
+        pose.calibrate(0);
+        setDistances(10,10,0);
+        checkDistances(10, 0);
+    }
+    
+    @Test
+    public void checkRollandPitch() {
+        setTilt(10, 15);
+        checkTilt(10, 15);
+        
+        setTilt(-10, -15);
+        checkTilt(-10, -15);
+    }    
+    
+    @Test
+    public void checkVelocity() {
+        setSpeeds(10, 10, 0);
+        checkRobotSpeeds(0, 10);
+        checkFieldSpeeds(0, 10);
+        
+        setSpeeds(10, 0, 0);
+        checkRobotSpeeds(0, 5);
+        checkFieldSpeeds(0, 5);
+    }
+    
+    @Test
+    public void turnRightAndCheckVelocity() {
+        pose.calibrate(0);
+        setSpeeds(10, 10, 0);
+        checkRobotSpeeds(0, 10);
+        checkFieldSpeeds(10, 0);
+    }
+    
 
+    private void setDistances(double left, double right, double middle)
+    {
+        ((MockEncoder)pose.leftLongitudinalEncoder).setDistance(left);
+        ((MockEncoder)pose.rightLongitudinalEncoder).setDistance(right);
+        ((MockEncoder)pose.middleTransverseEncoder).setDistance(middle);
+    }
+    
+    private void checkDistances(double x, double y)
+    {
+        assertEquals(x, pose.getPosition().x, 0.001);
+        assertEquals(y, pose.getPosition().y, 0.001);
+    }
+    
+    private void setSpeeds(double left, double right, double middle)
+    {
+        ((MockEncoder)pose.leftLongitudinalEncoder).setRate(left);
+        ((MockEncoder)pose.rightLongitudinalEncoder).setRate(right);
+        ((MockEncoder)pose.middleTransverseEncoder).setRate(middle);
+    }
+    
+    private void checkFieldSpeeds(double x, double y)
+    {
+        assertEquals(x, pose.getVelocityFieldRelative().x, 0.001);
+        assertEquals(y, pose.getVelocityFieldRelative().y, 0.001);
+    }
+    
+    private void checkRobotSpeeds(double x, double y)
+    {
+        assertEquals(x, pose.getVelocityRobotRelative().x, 0.001);
+        assertEquals(y, pose.getVelocityRobotRelative().y, 0.001);
+    }
+    
+    private void setTilt(double roll, double pitch) {
+        this.mockRobotIO.setGyroRoll(roll);
+        this.mockRobotIO.setGyroPitch(pitch);
+        
+    }
+    
+    private void checkTilt(double roll, double pitch) {
+        assertEquals(roll, pose.getTilt().x, 0.001);
+        assertEquals(pitch, pose.getTilt().y, 0.001);
+    }
 }
